6daa76f79536c13e6d68f635c50481d72e69ef12
Gonxalo <gonzalopr.94@gmail.com>
2023-06-03 20:42:44 -0300

    agrega componente footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/layout/navBar'
 import FeaturedEventCard from '@/components/home/featuredEventCard'
+import Footer from '@/components/layout/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar />
         <FeaturedEventCard />
+        <Footer />
         {children}
       </body>
     </html>
