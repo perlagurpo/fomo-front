7d440ea9e6a955dbfc0cd3cdd375d59137374c19
Gonxalo <gonzalopr.94@gmail.com>
2023-06-03 20:30:13 -0300

    fix importacion de FeaturedEventCard

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/layout/navBar'
+import FeaturedEventCard from '@/components/home/featuredEventCard'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <featuredEventCard />
+        <FeaturedEventCard />
         {children}
       </body>
     </html>
