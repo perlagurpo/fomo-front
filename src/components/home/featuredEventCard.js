23c8f7dbfb4aeb1dddb50eb81cd0e3c61810cd1f
Numa-diazdiva <manuel.diazd@hotmail.com>
2023-08-07 15:59:55 -0300

    Fix responsive y link a url dinámica

--- src/components/home/featuredEventCard.js
+++ src/components/home/featuredEventCard.js
@@ -1,25 +1,27 @@
 'use client';
+import Link from 'next/link';
 import React from 'react';
 
 const FeaturedEventCard = ({ event_id, event_name, event_img, start_date, event_location, ticket_price }) => {
   
   const startDate = start_date.split(" ");
-
   return (
-    <div className="max-w-sm max-h-sm bg-white rounded-lg drop-shadow-md hover:scale-102 cursor-pointer transition-all transition-400" key={event_id}>
-        <div className="flex-col justify-start items-start gap-2 flex">
-          <img className="w-[480px] h-[250.58px] rounded-tl-lg rounded-tr-lg object-cover" src={event_img} />
-        </div>
-        <div className="px-2 pt-3 pb-1"> 
-          <h5 className="px-1 mb-2 text-2xl font-bold tracking-tight text-fomo-sec-two">{event_name}</h5>
-          <p className="px-1 font-normal text-fomo-sec-two">{startDate[0] + ", " + startDate[1]}</p>
-          <div className="flex flex-row justify-start py-1">
-            <img className='max-h-6' src="/img/icons/location-icon.png" />
-            <p className="font-bold text-fomo-sec-two pl-1">{event_location}</p>
+    <Link href={`/evento/${event_id}`}>
+      <div className="max-w-sm max-h-sm bg-white rounded-lg drop-shadow-md hover:scale-102 cursor-pointer transition-all transition-400" key={event_id}>
+          <div className="flex-col justify-start items-start gap-2 flex">
+            <img className="w-[480px] h-[250.58px] rounded-tl-lg rounded-tr-lg object-cover" src={event_img} />
           </div>
-          <p className="px-1 font-normal text-fomo-sec-two">{ticket_price === 0 ? "Gratis" : "$ " + ticket_price}</p>
-        </div>
-    </div>
+          <div className="px-2 pt-3 pb-1"> 
+            <h5 className="px-1 mb-2 text-2xl font-bold tracking-tight text-fomo-sec-two">{event_name}</h5>
+            <p className="px-1 font-normal text-fomo-sec-two">{startDate[0] + ", " + startDate[1]}</p>
+            <div className="flex flex-row justify-start py-1">
+              <img className='max-h-6' src="/img/icons/location-icon.png" />
+              <p className="font-bold text-fomo-sec-two pl-1">{event_location}</p>
+            </div>
+            <p className="px-1 font-normal text-fomo-sec-two">{ticket_price === 0 ? "Gratis" : "$ " + ticket_price}</p>
+          </div>
+      </div>
+    </Link>
   );
 }
 
