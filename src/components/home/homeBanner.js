9be348940a6207dae59d887ffd1852616d4a3e3c
Franco Miceli <francomiceli.94@gmail.com>
2023-08-13 15:33:40 -0300

    fix defaultHeight state

--- src/components/home/homeBanner.js
+++ src/components/home/homeBanner.js
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from 'react';
-//import HomeService from '@/app/api/home.service';
+import HomeService from '@/app/api/home.service';
 
 const HomeBanner = () => {
+  const [defaultHeight, setDefaultHeight] = useState(0);
   const [carouselData, setCarouselData] = useState([]);
   const [activeIndex, setActiveIndex] = useState(0);
 
   useEffect(() => {
+    const handleResize = () => {
+      setDefaultHeight(window.innerWidth < 768 ? 360 : 221);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  
+  useEffect(() => {
     const fetchData = async () => {
-      // const data = await HomeService.getCarouselData();
-      const data = [
-        {
-          img_url: 'https://i.ibb.co/NVMXDqf/banner.jpg',
-          //img_url_mobile: 'https://i.ibb.co/NVMXDqf/banner.jpg',
-          redirect_link: 'https://www.google.com/search?channel=fs&client=ubuntu&q=react+tailwind+carousel'
-        },
-        {
-          img_url: 'https://i.ibb.co/k4phbF8/banner-1.jpg',
-          //img_url_mobile: 'https://i.ibb.co/NVMXDqf/banner.jpg',
-          redirect_link: 'https://www.google.com/search?channel=fs&client=ubuntu&q=react+tailwind+carousel'
-        },
-      ]
+      const data = await HomeService.getCarouselData();
       setCarouselData(data);
     };
 
@@ -39,22 +41,25 @@ const HomeBanner = () => {
   }
 
 return (
-  <div className="w-full max-w-screen-xl mx-auto relative" style={{ minHeight: '221px' }}>
+  <div className="w-full max-w-screen-xl mx-auto relative" style={{ minHeight: `${defaultHeight}px`, maxHeight: `${defaultHeight}px` }}>
     {carouselData.map((item, index) => (
       <a
         key={index}
-        href={item.redirect_link}
+        href={item.link_button}
         target="_blank"
         rel="noopener noreferrer"
         className={`absolute w-full h-221 transform transition-transform ${
           index === activeIndex ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-full'
         }`}
-        style={{ backgroundImage: `url(${item.img_url})`, zIndex: index === activeIndex ? 1 : 0 }}
+        style={{ backgroundImage: `url(${
+              window.innerWidth < 768 ? 'http://18.231.76.133' + item.image_short_mobile : 'http://18.231.76.133' + item.image_short
+            })`, zIndex: index === activeIndex ? 1 : 0, minHeight: `${defaultHeight}px`, maxHeight: `${defaultHeight}px` }}
       >
         <img
-          src={item.img_url}
+          src={window.innerWidth < 768 ? 'http://18.231.76.133' + item.image_short_mobile : 'http://18.231.76.133' + item.image_short}
           alt="Banner"
           className="h-221 w-full object-cover"
+          style={{minHeight: `${defaultHeight}px`, maxHeight: `${defaultHeight}px` }}
         />
       </a>
     ))}
